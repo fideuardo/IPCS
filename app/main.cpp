776f4b79e4c3d3240364d6c2a53ab22250c54019
fideuardo <fideuardo@gmail.com>
2025-03-14 16:07:58 -0600

    Task Manager Test

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,24 @@
+#include "task.h"
+#include "TaskManager.h"
+#include "TaskExecutor.h"
 #include <iostream>
 
-using namespace std;
+void initFunc1() { std::cout << "Init func 1\n"; }
+void runFunc1() { std::cout << "Run func 1\n"; }
 
 int main() {
-    cout << "Hello, World!" << endl;
+    void* initFuncs[] = {reinterpret_cast<void*>(&initFunc1), nullptr};
+    void* runFuncs[] = {reinterpret_cast<void*>(&runFunc1), nullptr};
+
+    Task task1(1, 1000, initFuncs, runFuncs, nullptr, nullptr);
+    Task task2(2, 2000, nullptr, runFuncs, nullptr, nullptr);
+
+    TaskManager manager;
+    manager.addTask(task1);
+    manager.addTask(task2);
+
+    TaskExecutor executor;
+    executor.executeTasks(manager);
+
     return 0;
 }
- 
